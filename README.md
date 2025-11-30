822c4dfaec035e46b420f67eaf911d9fdd65c185
tomas-rojo <tomasrojo21@hotmail.com>
2025-11-30 02:29:36 +0100

    Include nix shell configuration

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Homelab GitOps
+
+## Nix Shell
+
+Generate kubeconfig when running minikube
+```sh
+kubectl config view --flatten --minify > kubeconfig-docker
+```
+
+Update kubeconfig with `minikube ip`
+```sh
+sed -i 's|https://127.0.0.1:[0-9]*|https://KUBE_CLUSTER_IP:8443|g' kubeconfig-docker
+```
+
+Create a docker volume to store all nix packages (this will make startup faster afters the 1st one)
+
+```sh
+docker volume create nix-store
+```
+
+Run the nix shell using docker
+```sh
+docker run -it --rm \
+    --network=host \
+    -v $(pwd):/workspace \
+    -v $(pwd)/kubeconfig-docker:/root/.kube/config:ro \
+    -v nix-store:/nix \
+    -w /workspace \
+    nixos/nix \
+    nix-shell
+```
---
+++ shell.nix
@@ -0,0 +1,48 @@
+# Configuration for `nix-shell` to setup a development environment.
+# See https://nixos.org
+
+let
+  pkgs = (import <nixpkgs>) {
+    config = {
+      allowUnfree = true;  # Required for Terraform >= 1.6.0
+    };
+  };
+  appDir = builtins.toString ./.;
+in
+  pkgs.mkShell {
+    name = "Tomas Homelab Shell";
+
+    buildInputs = with pkgs; [
+      terraform
+      kubectl
+      kubeseal
+      argocd
+      git
+      curl
+      jq
+    ];
+
+    shellHook = ''
+      USER_POLICY_PATH="$HOME/.config/containers"
+      USER_POLICY_FILE="$USER_POLICY_PATH/policy.json"
+      GLOBAL_POLICY_FILE="/etc/containers/policy.json"
+      if [[ ! -e $USER_POLICY_FILE && ! -e $GLOBAL_POLICY_FILE ]]; then
+        echo "Create empty $USER_POLICY_FILE"
+        mkdir -p $USER_POLICY_PATH
+        echo '{"default": [{"type": "insecureAcceptAnything"}]}' | jq > $USER_POLICY_FILE
+      fi
+
+      export PS1="[HomeLab - Tomas] $ "
+
+      echo ""
+      echo "Tool versions:"
+      echo "  • Terraform:  $(terraform version | head -n1)"
+      echo "  • kubectl:    $(kubectl version --client | awk 'NR==1{printf "%s - ", $0} NR==2{print $0}')"
+      echo "  • kubeseal:   $(kubeseal --version 2>&1 | head -n1)"
+      echo "  • argocd:     $(argocd version --client --short 2>/dev/null || echo 'argocd CLI')"
+      echo ""
+
+      alias k="kubectl"
+      alias tf="terraform"
+    '';
+  }
